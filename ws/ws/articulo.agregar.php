fbf8400012175c3b32b425f633f086c798903f95
Juan Eduardo Ballena Dioses <eduardo_bd10@hotmail.com>
2024-05-25 13:07:13 -0500

    Update articulo.agregar.php

--- ws/ws/articulo.agregar.php
+++ ws/ws/articulo.agregar.php
@@ -4,4 +4,4 @@ require_once '../logica/Articulo.clase.php';
 require_once '../util/funciones/Funciones.clase.php';
 require_once './token.validar.php';
 
-    move_uploaded_file($_FILES["image"]["tmp_name"], '../image/'.basename($_FILES["image"]["name"]));
+    move_uploaded_file($_FILES["image"]["tmp_name"], $_SERVER["DOCUMENT_ROOT"].'ws/image/'.basename($_FILES["image"]["name"]));
