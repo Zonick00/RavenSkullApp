17b897c35c0d1754975fad1073224e708b339ed3
Juan Eduardo Ballena Dioses <eduardo_bd10@hotmail.com>
2024-05-25 13:16:58 -0500

    error hhtps

--- ws/ws/articulo.agregar.php
+++ ws/ws/articulo.agregar.php
@@ -4,4 +4,4 @@ require_once '../logica/Articulo.clase.php';
 require_once '../util/funciones/Funciones.clase.php';
 require_once './token.validar.php';
 
-    move_uploaded_file($_FILES["image"]["tmp_name"],Funciones::$DIRECCION_WEB_SERVICE.'/image/'.basename($_FILES["image"]["name"]));
+    move_uploaded_file($_FILES["image"]["tmp_name"],Funciones::$DIRECCION_WEB_SERVICE.'image/'.basename($_FILES["image"]["name"]));
