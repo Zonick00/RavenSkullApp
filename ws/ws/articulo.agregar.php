5666d3a38f46ac425a64f23294639652a8c21e37
Juan Eduardo Ballena Dioses <eduardo_bd10@hotmail.com>
2024-05-25 12:30:45 -0500

    prueba_2

--- ws/ws/articulo.agregar.php
+++ ws/ws/articulo.agregar.php
@@ -7,9 +7,18 @@ require_once './token.validar.php';
 /*if (! isset($_FILE["image"])){
     Funciones::imprimeJSON("Falta completar los datos requeridos");
     exit();
-}*/
+}
 
 $image = $_FILE["image"];
 $folder = '../image/'.$image.'.jpg';
 
 Funciones::cargarArchivo($image, $folder);
+ */
+
+ $filename = $_FILES["choosefile"]["name"];
+
+    $tempname = $_FILES["choosefile"]["tmp_name"];  
+
+    $folder = "image/".$filename;
+    
+    move_uploaded_file($tempname, $folder);
