ab32448ef4dc367a3c1ef6e2e11487bf2ce5dc1b
Juan Eduardo Ballena Dioses <eduardo_bd10@hotmail.com>
2024-05-25 12:26:43 -0500

    Update articulo.agregar.php

--- ws/ws/articulo.agregar.php
+++ ws/ws/articulo.agregar.php
@@ -4,10 +4,10 @@ require_once '../logica/Articulo.clase.php';
 require_once '../util/funciones/Funciones.clase.php';
 require_once './token.validar.php';
 
-if (! isset($_FILE["image"])){
+/*if (! isset($_FILE["image"])){
     Funciones::imprimeJSON("Falta completar los datos requeridos");
     exit();
-}
+}*/
 
 $image = $_FILE["image"];
 $folder = '../image/'.$image.'.jpg';
