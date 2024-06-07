3fd90ed406a23bdbd06d49a78cff45db3cefcd47
Juan Eduardo Ballena Dioses <eduardo_bd10@hotmail.com>
2024-06-07 10:02:43 -0500

    agregado producto.actualizar.php

--- ws/logica/Producto.clase.php
+++ ws/logica/Producto.clase.php
@@ -48,5 +48,34 @@ class Producto extends Conexion{
             throw $exc;
         }
     }
+    
+    public function actualizarProducto($p_productoId, $p_name, $p_price, $p_wholesale, $p_sold, $p_stock, $p_image, $p_category) {
+        try{
+            $sql = "UPDATE public.products
+                    SET product_name=:p_name, 
+                        product_price=:p_price, 
+                        product_wholesale=:p_wholesale, 
+                        product_sold=:p_sold, 
+                        product_stock=:p_stock, 
+                        product_image=:p_image, 
+                        category_id=:p_category
+                    WHERE product_id = :p_productoId;
+                    select * from public.products WHERE product_id = :p_productoId";
+                    
+            $sentencia = $this->dblink->prepare($sql);
+            $sentencia->execute(array(":p_name"=> $p_name, 
+                                      ":p_price"=> $p_price, 
+                                      ":p_wholesale"=> $p_wholesale,
+                                      ":p_sold"=> $p_sold,
+                                      ":p_stock"=> $p_stock,
+                                      ":p_image"=> $p_image,
+                                      ":p_category"=> $p_category,
+                                      ":p_productoId"=> $p_productoId));
+            return $sentencia->fetch(PDO::FETCH_ASSOC);
+            
+        } catch (Exception $exc) {
+            throw $exc;
+        }
+    }
    
 }
---
+++ ws/ws/producto.actualizar.php
@@ -0,0 +1,36 @@
+<?php
+require_once '../logica/Producto.clase.php';
+require_once '../util/funciones/Funciones.clase.php';
+require_once './token.validar.php';
+
+if (! isset($_POST["p_productoId"]) || ! isset($_POST["p_name"]) || ! isset($_POST["p_price"]) || ! isset($_POST["p_wholesale"]) || ! isset($_POST["p_sold"]) || ! isset($_POST["p_stock"]) || ! isset($_POST["p_image"]) || ! isset($_POST["p_category"])){
+    Funciones::imprimeArrayJSON(500,"Falta completar los datos requeridos", "");
+    exit();
+}
+
+if (! isset($_POST["token"])){
+    Funciones::imprimeArrayJSON(500, "Debe especificar un token", "");
+    exit();
+}
+    
+try {
+    if(validarToken($_POST["token"])){
+        //si devuelve true, quiere decir q el token es valido
+        $objProducto = new Producto();
+        
+        $p_productoId = $_POST["p_productoId"];
+        $p_name = $_POST["p_name"];
+        $p_price = $_POST["p_price"];
+        $p_wholesale = $_POST["p_wholesale"];
+        $p_sold = $_POST["p_sold"];
+        $p_stock = $_POST["p_stock"];
+        $p_image = $_POST["p_image"];
+        $p_category = $_POST["p_category"];
+        
+        $resultado = $objProducto->actualizarProducto($p_productoId, $p_name, $p_price, $p_wholesale, $p_sold, $p_stock, $p_image, $p_category);
+        
+        Funciones::imprimeArrayJSON(200, "Producto actualizado con éxito", $resultado);
+    }                                           
+} catch (Exception $exc) {
+    Funciones::imprimeArrayJSON(500,$exc->getMessage(),"");
+}
