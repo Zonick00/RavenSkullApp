91d347b679a0bae0e3f9a273f4b8cabce5d34b0e
Juan Eduardo Ballena Dioses <eduardo_bd10@hotmail.com>
2024-09-07 11:38:14 -0500

    Update configuracion.php

--- ws/datos/configuracion.php
+++ ws/datos/configuracion.php
@@ -1,7 +1,7 @@
 <?php
  
-define("SERVIDOR_BD", "roundhouse.proxy.rlwy.net");
+define("SERVIDOR_BD", "junction.proxy.rlwy.net");
 define("USUARIO_BD", "postgres");
-define("CLAVE_BD", "CLHLUIkhoaJXThRvlvQvdRgGERrmzUQN");
-define("PUERTO_BD", "20526");
+define("CLAVE_BD", "nkiUvWmYLMCJlkqeMKmiwQfdvhmlESoJ");
+define("PUERTO_BD", "54271");
 define("NOMBRE_BD", "railway");
