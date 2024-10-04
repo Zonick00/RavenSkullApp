185612650b1a7be9361bed80d0d27ce0c835943f
Juan Eduardo Ballena Dioses <eduardo_bd10@hotmail.com>
2024-10-04 09:00:24 -0500

    bd

--- ws/datos/configuracion.php
+++ ws/datos/configuracion.php
@@ -3,5 +3,5 @@
 define("SERVIDOR_BD", "junction.proxy.rlwy.net");
 define("USUARIO_BD", "postgres");
 define("CLAVE_BD", "nkiUvWmYLMCJlkqeMKmiwQfdvhmlESoJ");
-define("PUERTO_BD", "54271");
+define("PUERTO_BD", "11283");
 define("NOMBRE_BD", "railway");
